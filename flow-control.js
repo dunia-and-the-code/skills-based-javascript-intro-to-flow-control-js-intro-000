40935cf19195abe652f0c4c6649bae048290ff55
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-01-30 17:59:28 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,8 +1,11 @@
 function basicTeenager(age) {
 if (age >= 13 || age <= 19) {
   return "You are a teenager!";
+} else if (age <= 12 || age > 19) {
+  return "You are not a teenager";
 }
 }
+
 
 function teenager(age) {
 if (age >=13 || age <= 19) {
