1b4ad76bfadfbf45499ec4e0e67d190a1eb6b4b8
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-01-30 17:54:36 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,41 @@
 function basicTeenager(age) {
-
+if (age >= 13 || age <= 19) {
+  return "You are a teenager!";
+}
 }
 
 function teenager(age) {
-
+if (age >=13 || age <= 19) {
+return "You are a teenager!";
+} else {
+  return "You are not a teenager";
+} 
 }
 
 function ageChecker(age) {
-
+if (age <= 12) {
+return "You are a kid";
+} else if (age >= 20) {
+return "You are a grownup";
+} else if (age >= 13 || age <= 19) {
+return "You are a teenager!"; 
+}
 }
 
-function ternaryTeenager(age) {
-
+function ternaryTeenager(age) { 
+return age >= 13 || age <= 19 ?
+"You are a teenager" :
+"You are not a teenager";
 }
+
 
 function switchAge(age) {
-
+switch(age) {
+  case  13, 14, 15, 15, 17, 18, 19 :
+    console.log("You are a teenager");
+    break;
+    default:
+    console.log("You have an age");
 }
+}
+
